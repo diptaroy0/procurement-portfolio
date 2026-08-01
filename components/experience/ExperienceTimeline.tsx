117f00ae2ce27b9c063132a54b3e2159bec3b620
Dipta Roy <diptaroy0@gmail.com>
2026-08-01 07:05:44 +0000

    ...

--- components/experience/ExperienceTimeline.tsx
+++ components/experience/ExperienceTimeline.tsx
@@ -5,13 +5,31 @@ import ExperienceCard from "./ExperienceCard";
 
 export default function ExperienceTimeline() {
   return (
-    <div className="space-y-12">
-      {experiences.map((experience) => (
-        <ExperienceCard
-          key={`${experience.company}-${experience.title}-${experience.period}`}
-          experience={experience}
-        />
-      ))}
+    <div className="relative mx-auto max-w-6xl">
+      <div
+        aria-hidden="true"
+        className="
+          absolute
+          left-6
+          top-0
+          bottom-0
+          w-px
+          bg-gradient-to-b
+          from-cyan-400
+          via-cyan-500/70
+          to-transparent
+          md:left-12
+        "
+      />
+
+      <div className="space-y-12 lg:space-y-16">
+        {experiences.map((experience) => (
+          <ExperienceCard
+            key={`${experience.company}-${experience.title}-${experience.period}`}
+            experience={experience}
+          />
+        ))}
+      </div>
     </div>
   );
 }
