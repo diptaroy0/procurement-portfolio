6bb4da54aec0bd2ead6a13db311751e8dde1d49b
Dipta Roy <diptaroy0@gmail.com>
2026-08-01 05:04:49 +0000

    ...

--- components/workflow/ProcurementWorkflow.tsx
+++ components/workflow/ProcurementWorkflow.tsx
@@ -37,7 +37,7 @@ export default function ProcurementWorkflow() {
 
             bg-cyan-500/8
 
-            blur-[130px]
+            blur-0
           "
         />
 
@@ -54,7 +54,7 @@ export default function ProcurementWorkflow() {
 
             bg-blue-500/8
 
-            blur-[140px]
+            blur-0
           "
         />
       </div>
