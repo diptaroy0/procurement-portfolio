6b6195899831f61c7d93a649a233676c5214b34d
Dipta Roy <diptaroy0@gmail.com>
2026-08-01 05:23:42 +0000

    ...

--- components/experience/ExperienceTimeline.tsx
+++ components/experience/ExperienceTimeline.tsx
@@ -1,65 +1,16 @@
-"use client";
-
-import { motion } from "framer-motion";
-
-import { experiences } from "./experienceData";
-import ExperienceCard from "./ExperienceCard";
-
 export default function ExperienceTimeline() {
   return (
-    <div className="relative mx-auto max-w-6xl">
-      {/* Vertical Timeline */}
+    <div className="space-y-6">
+      <div className="rounded-xl bg-white p-6 text-black">
+        Card 1
+      </div>
 
-      <motion.div
-        aria-hidden="true"
-        initial={{ height: 0, opacity: 0 }}
-        whileInView={{ height: "100%", opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{
-          duration: 1,
-          ease: "easeOut",
-        }}
-        className="
-          absolute
-          left-6
-          top-0
-          bottom-0
-          w-px
+      <div className="rounded-xl bg-white p-6 text-black">
+        Card 2
+      </div>
 
-          bg-gradient-to-b
-          from-cyan-400
-          via-cyan-500/70
-          via-60%
-          to-transparent
-
-          md:left-12
-        "
-      />
-
-      {/* Timeline Items */}
-
-      <div className="space-y-12 lg:space-y-16">
-        {experiences.map((experience, index) => (
-          <motion.div
-            key={`${experience.company}-${experience.title}-${experience.period}`}
-            initial={{
-              opacity: 0,
-              y: 40,
-            }}
-            whileInView={{
-              opacity: 1,
-              y: 0,
-            }}
-            viewport={{ once: true }}
-            transition={{
-              duration: 0.6,
-              delay: index * 0.15,
-              ease: "easeOut",
-            }}
-          >
-            <ExperienceCard experience={experience} />
-          </motion.div>
-        ))}
+      <div className="rounded-xl bg-white p-6 text-black">
+        Card 3
       </div>
     </div>
   );
