2b446e31aa197430e0728e8a3375d4627d283bc1
Dipta Roy <diptaroy0@gmail.com>
2026-08-01 06:18:26 +0000

    ...

--- components/experience/ExperienceTimeline.tsx
+++ components/experience/ExperienceTimeline.tsx
@@ -1,64 +1,33 @@
 "use client";
 
-import { motion } from "framer-motion";
-
 import { experiences } from "./experienceData";
 import ExperienceCard from "./ExperienceCard";
 
 export default function ExperienceTimeline() {
   return (
     <div className="relative mx-auto max-w-6xl">
-      {/* Vertical Timeline */}
-
-      <motion.div
+      {/* Static Vertical Line */}
+      <div
         aria-hidden="true"
-        initial={{ height: 0, opacity: 0 }}
-        whileInView={{ height: "100%", opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{
-          duration: 1,
-          ease: "easeOut",
-        }}
         className="
           absolute
           left-6
           top-0
           bottom-0
           w-px
-
-          bg-gradient-to-b
-          from-cyan-400
-          via-cyan-500/70
-          via-60%
-          to-transparent
-
+          bg-cyan-400/40
           md:left-12
         "
       />
 
       {/* Timeline Items */}
-
       <div className="space-y-12 lg:space-y-16">
-        {experiences.map((experience, index) => (
-          <motion.div
+        {experiences.map((experience) => (
+          <div
             key={`${experience.company}-${experience.title}-${experience.period}`}
-            initial={{
-              opacity: 0,
-              y: 40,
-            }}
-            whileInView={{
-              opacity: 1,
-              y: 0,
-            }}
-            viewport={{ once: true }}
-            transition={{
-              duration: 0.6,
-              delay: index * 0.15,
-              ease: "easeOut",
-            }}
           >
             <ExperienceCard experience={experience} />
-          </motion.div>
+          </div>
         ))}
       </div>
     </div>
