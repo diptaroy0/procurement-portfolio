1afa8287f6a25832ecb4ec297652b3035dff3b0a
Dipta Roy <diptaroy0@gmail.com>
2026-08-01 06:22:07 +0000

    ...

--- components/experience/ExperienceTimeline.tsx
+++ components/experience/ExperienceTimeline.tsx
@@ -1,35 +1,28 @@
-"use client";
-
 import { experiences } from "./experienceData";
-import ExperienceCard from "./ExperienceCard";
 
 export default function ExperienceTimeline() {
   return (
-    <div className="relative mx-auto max-w-6xl">
-      {/* Static Vertical Line */}
-      <div
-        aria-hidden="true"
-        className="
-          absolute
-          left-6
-          top-0
-          bottom-0
-          w-px
-          bg-cyan-400/40
-          md:left-12
-        "
-      />
+    <div className="space-y-8">
+      {experiences.map((exp) => (
+        <div
+          key={exp.title}
+          className="rounded-3xl bg-white p-8 text-black"
+        >
+          <h2 className="text-2xl font-bold">
+            {exp.title}
+          </h2>
 
-      {/* Timeline Items */}
-      <div className="space-y-12 lg:space-y-16">
-        {experiences.map((experience) => (
-          <div
-            key={`${experience.company}-${experience.title}-${experience.period}`}
-          >
-            <ExperienceCard experience={experience} />
-          </div>
-        ))}
-      </div>
+          <p>{exp.company}</p>
+
+          <p>{exp.period}</p>
+
+          <ul className="mt-5 list-disc pl-5">
+            {exp.responsibilities.map((r) => (
+              <li key={r}>{r}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
