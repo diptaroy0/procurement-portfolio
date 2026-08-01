d1b7bd18fc43a875d733bb0ce3c911d77deecc36
Dipta Roy <diptaroy0@gmail.com>
2026-08-01 05:30:27 +0000

    ...

--- components/experience/ExperienceTimeline.tsx
+++ components/experience/ExperienceTimeline.tsx
@@ -1,17 +1,28 @@
+import { experiences } from "./experienceData";
+
 export default function ExperienceTimeline() {
   return (
-    <div className="space-y-6">
-      <div className="rounded-xl bg-white p-6 text-black">
-        Card 1
-      </div>
+    <div className="space-y-8">
+      {experiences.map((exp) => (
+        <div
+          key={exp.title}
+          className="rounded-3xl bg-white p-8 text-black"
+        >
+          <h2 className="text-2xl font-bold">
+            {exp.title}
+          </h2>
 
-      <div className="rounded-xl bg-white p-6 text-black">
-        Card 2
-      </div>
+          <p>{exp.company}</p>
 
-      <div className="rounded-xl bg-white p-6 text-black">
-        Card 3
-      </div>
+          <p>{exp.period}</p>
+
+          <ul className="mt-5 list-disc pl-5">
+            {exp.responsibilities.map((r) => (
+              <li key={r}>{r}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
